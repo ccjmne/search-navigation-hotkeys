2bab7881753ee40f32fe8e65d2340d4e09e47079
ccjmne <ccjmne@gmail.com>
2018-09-29 17:34:02 +0200

    Extract relevant info from package.json into userscript meta tags

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+'use strict';
+
 const MinifyPlugin = require('babel-minify-webpack-plugin');
 const WrapperPlugin = require('wrapper-webpack-plugin');
 
 const UserscriptMeta = require('userscript-meta');
+const p = require('./package.json');
 
 module.exports = {
   optimization: { minimize: false },
@@ -21,11 +24,11 @@ module.exports = {
     new MinifyPlugin(),
     new WrapperPlugin({
       header: UserscriptMeta.stringify({
-        name: 'Google Search Hotkeys',
-        version: '2.0.0',
-        description: 'Navigate results from Google searches without ever lifting your hands off the keyboard!',
-        author: 'ccjmne <ccjmne@gmail.com> (https://github.com/ccjmne)',
-        namespace: 'https://github.com/ccjmne',
+        name: p.name.split(/-/).map(v => v.charAt(0).toUpperCase() + v.slice(1)).join(' '), // kebab to title case
+        version: p.version,
+        description: p.description,
+        author: p.author,
+        namespace: p.homepage,
         include: '*://www.google.tld/search*',
         downloadURL: 'https://github.com/ccjmne/google-search-hotkeys/master/dist/main.js',
         grant: 'none'
