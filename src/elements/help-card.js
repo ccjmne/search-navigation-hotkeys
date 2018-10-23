e914304497c5fea2baf9a7526711fac2f73dcdd8
ccjmne <ccjmne@gmail.com>
2018-10-23 17:22:03 +0200

    Update help-card component

--- src/elements/help-card.js
+++ src/elements/help-card.js
@@ -9,38 +9,40 @@ export const helpCard = create({ id: 'ccjmne-gsh-help-card', contents: `
     </div>
     <table></table>` });
 
-const metaChars = { '..': ' to ', '-': '+', '|': ' or ', '[': ' [', ']': '] ' };
-const addons = { Up: '↑', Down: '↓', Left: '←', Right: '→', Ctrl: '⌃', Shift: '⇧', Space: '⎵', Enter: '↲' };
+const metaChars = { '..': ' to ', '-': '+', '|': ' or ', '[': ' [', ']': '] ', 'w/': 'add ' };
+const addons = { Up: '↑', Down: '↓', Left: '←', Right: '→', Ctrl: '⌃', Shift: '⇧', Space: '⎵', Enter: '↲', 'Escape': '␛' };
 
 const table = helpCard.querySelector('table');
 
 [
   [
-    { desc: `Focus [previous] result`, hotkey: `Up|j` },
-    { desc: `Focus [next] result`, hotkey: `Down|k` },
+    { desc: `Focus [previous] result`, hotkey: `Up|k` },
+    { desc: `Focus [next] result`, hotkey: `Down|j` },
     { desc: `Navigate to [previous] page`, hotkey: `Left|h` },
     { desc: `Navigate to [next] page`, hotkey: `Right|l` }
   ],
   [
-    { desc: `[Open] focused`, hotkey: `Space|Enter` },
-    { desc: `[Open] focused in [new tab]`, hotkey: `Ctrl-[Space|Enter]` },
-    { desc: `Open #[1] to #[9]`, hotkey: `1..9` },
-    { desc: `Open #[1] to #[9] in [new tab]`, hotkey: `Ctrl-[1..9]` }
+    { desc: `Open [focused] result`, hotkey: `Space|Enter` },
+    { desc: `in [new tab]`, hotkey: `w/Ctrl`, indent: 1 },
+    { desc: `and [follow]`, hotkey: `w/Ctrl-Shift`, indent: 2 },
+    { desc: `Open result #[1] to #[9]`, hotkey: `1..9` }
   ],
   [
     { desc: `Focus [search] field`, hotkey: `/` },
+    { desc: `Enter [filter & sort] mode`, hotkey: `Ctrl-/` }
+  ],
+  [
     { desc: `Search [all]`, hotkey: `a` },
     { desc: `Search [videos]`, hotkey: `v` },
     { desc: `Search [images]`, hotkey: `i` },
     { desc: `Search [news]`, hotkey: `n` }
   ],
   [
-    { desc: `Enter [filter/sort] mode`, hotkey: `Ctrl-/` },
-    { desc: `[Show] help`, hotkey: `Shift-?` },
+    { desc: `[Show] help`, hotkey: `?` },
     { desc: `[Close] help`, hotkey: `Escape` }
   ]
 ].forEach(block => block.forEach((op, idx) => table.appendChild(create({ type: 'tr', classes: idx === 0 ? ['ccjmne-gsh-new-section'] : [], contents: `
-    <td>${ op.desc.replace(/\[([^\]]+)\]/g, (unused, d) => `<em>${ d }</em>`) }</td>
+    <td>${ op.indent ? `<div class="ccjmne-gsh-indent ccjmne-gsh-indent-${ op.indent }"></div>` : '' }${ op.desc.replace(/\[([^\]]+)\]/g, (unused, d) => `<em>${ d }</em>`) }</td>
     <td>${ op.hotkey
       /* tokenise  */.split(new RegExp((s => `(?=${ s })|(?<=${ s })`)(Object.keys(metaChars).map(k => k.replace(/./g, c => '\\' + c)).join('|'))))
       /* transform */.map(s => metaChars[s] || `<kbd>${ s }${ addons[s] ? `<span class="kbd-addon">${ addons[s] }</span>` : '' }</kbd>`)
