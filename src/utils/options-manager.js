b61757ed6c41a4b080f7ccf99ba3649b9968fff6
ccjmne <ccjmne@gmail.com>
2018-11-03 00:01:25 +0100

    Implement options-manager to access user options

---
+++ src/utils/options-manager.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/* globals chrome */
+
+const defaults = { 'key:open-link': ' ', 'feature:whats-this': true, 'mode:secondary-navigation': 'jkhl' };
+const pick = (keys, obj = defaults) => Object.keys(obj).filter(k => ~keys.indexOf(k)).reduce((acc, k) => Object.assign(acc, {
+  [k]: obj[k]
+}), {});
+
+function get(opts = Object.keys(defaults)) {
+  if (typeof chrome === 'undefined' || typeof chrome.storage === 'undefined' || typeof chrome.storage.sync === 'undefined') {
+    return Promise.resolve(pick(opts));
+  }
+
+  return new Promise(resolve => chrome.storage.sync.get(pick(opts), data =>
+    typeof opts === 'string' ? resolve(data[opts]) : resolve(data)
+  ));
+}
+
+export { get, defaults };
