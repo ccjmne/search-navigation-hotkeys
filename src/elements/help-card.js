500fb0b5b81ccaaf1478d16c51d24ecfdfb58983
ccjmne <ccjmne@gmail.com>
2018-11-05 22:06:22 +0100

    Enhance help-card to use user options

--- src/elements/help-card.js
+++ src/elements/help-card.js
@@ -14,38 +14,42 @@ const addons = { Up: '↑', Down: '↓', Left: '←', Right: '→', Ctrl: '⌃',
 
 const table = helpCard.querySelector('table');
 
-[
+getOpts(['mode:secondary-navigation', 'key:open-link']).then(options => {
+  const udlr = options['mode:secondary-navigation'];
+  const open = options['key:open-link'] === ' ' ? 'Space' : options['key:open-link'];
+
   [
-    { desc: `Focus [previous] result`, hotkey: `Up|k` },
-    { desc: `Focus [next] result`, hotkey: `Down|j` },
-    { desc: `Navigate to [previous] page`, hotkey: `Left|h` },
-    { desc: `Navigate to [next] page`, hotkey: `Right|l` }
-  ],
-  [
-    { desc: `Open [focused] result`, hotkey: `Space|Enter` },
-    { desc: `in [new tab]`, hotkey: `w/Ctrl`, indent: 1 },
-    { desc: `and [follow]`, hotkey: `w/Ctrl-Shift`, indent: 2 },
-    { desc: `Open result #[1] to #[9]`, hotkey: `1..9` }
-  ],
-  [
-    { desc: `Focus [search] field`, hotkey: `/` },
-    { desc: `Enter [filter & sort] mode`, hotkey: `Ctrl-/` }
-  ],
-  [
-    { desc: `Search [all]`, hotkey: `a` },
-    { desc: `Search [videos]`, hotkey: `v` },
-    { desc: `Search [images]`, hotkey: `i` },
-    { desc: `Search [news]`, hotkey: `n` }
-  ],
-  [
-    { desc: `[Show] help`, hotkey: `?` },
-    { desc: `[Close] help`, hotkey: `Escape` }
-  ]
-].forEach(block => block.forEach((op, idx) => table.appendChild(create({ type: 'tr', classes: idx === 0 ? ['ccjmne-snh-new-section'] : [], contents: `
-    <td>${ op.indent ? `<div class="ccjmne-snh-indent ccjmne-snh-indent-${ op.indent }"></div>` : '' }${ op.desc.replace(/\[([^\]]+)\]/g, (unused, d) => `<em>${ d }</em>`) }</td>
-    <td>${ op.hotkey
-      /* tokenise  */.split(new RegExp((s => `(?=${ s })|(?<=${ s })`)(Object.keys(metaChars).map(k => k.replace(/./g, c => '\\' + c)).join('|'))))
-      /* transform */.map(s => metaChars[s] || `<kbd>${ s }${ addons[s] ? `<span class="kbd-addon">${ addons[s] }</span>` : '' }</kbd>`)
-      /* wrap up   */.join('') }</td>` }))));
+    [
+      { desc: `Focus [previous] result`, hotkey: `Up|${ udlr.charAt(0) }` },
+      { desc: `Focus [next] result`, hotkey: `Down|${ udlr.charAt(1) }` },
+      { desc: `Navigate to [previous] page`, hotkey: `Left|${ udlr.charAt(2) }` },
+      { desc: `Navigate to [next] page`, hotkey: `Right|${ udlr.charAt(3) }` }
+    ],
+    [
+      { desc: `Open [focused] result`, hotkey: `Enter|${ open }` },
+      { desc: `in [new tab]`, hotkey: `w/Ctrl`, indent: 1 },
+      { desc: `and [follow]`, hotkey: `w/Ctrl-Shift`, indent: 2 },
+      { desc: `Open result #[1] to #[9]`, hotkey: `1..9` }
+    ],
+    [
+      { desc: `Focus [search] field`, hotkey: `/` },
+      { desc: `Enter [filter & sort] mode`, hotkey: `Ctrl-/` }
+    ],
+    [
+      { desc: `switch to [all]`, hotkey: `a` },
+      { desc: `switch to [videos]`, hotkey: `v` },
+      { desc: `switch to [images]`, hotkey: `i` },
+      { desc: `switch to [news]`, hotkey: `n` }
+    ],
+    [
+      { desc: `[Show] help`, hotkey: `?` },
+      { desc: `[Close] help`, hotkey: `Escape` }
+    ]
+  ].forEach(block => block.forEach((op, idx) => table.appendChild(create({ type: 'tr', classes: idx === 0 ? ['ccjmne-snh-new-section'] : [], contents: `
+  <td>${ op.indent ? `<div class="ccjmne-snh-indent ccjmne-snh-indent-${ op.indent }"></div>` : '' }${ op.desc.replace(/\[([^\]]+)\]/g, (unused, d) => `<em>${ d }</em>`) }</td>
+  <td>${ op.hotkey
+    /* tokenise  */.split(new RegExp((s => `(?=${ s })|(?<=${ s })`)(Object.keys(metaChars).map(k => k.replace(/./g, c => '\\' + c)).join('|'))))
+    /* transform */.map(s => metaChars[s] || `<kbd class="ccjmne-snh-kbd">${ s }${ addons[s] ? `<span class="kbd-addon">${ addons[s] }</span>` : '' }</kbd>`)
+    /* wrap up   */.join('') }</td>` }))));
 
 helpCard.addEventListener('click', e => e.stopPropagation());
