6b611e56ae6a86223c3ad69be30e83aa37144a36
ccjmne <ccjmne@gmail.com>
2018-11-06 00:43:48 +0100

    Implement onceMore, like onceSome w/ delay

--- src/utils/once.js
+++ src/utils/once.js
@@ -1,23 +1,26 @@
 'use strict';
 
+const maxDuration = 1000;
+
 const interval = 50;
-const maxTries = 1000 / interval;
+const maxTries = maxDuration / interval;
+const moreDelay = maxDuration / 4;
 
 class Once {
-  constructor(selector = '', selectAll = false) {
-    this.all = !!selectAll;
+  constructor(selector = '', selectAll = false, delay = 0) {
     this.promise = new Promise((resolve, reject) => {
       this.resolve = resolve;
       this.reject = reject;
     });
 
+    this._all = !!selectAll;
     this._s = selector;
     this._count = 0;
-    return this._test() || (this._ = setInterval(() => (res => res && clearInterval(this._))(this._test()), interval));
+    return setTimeout(() => this._test() || (this._ = setInterval(() => (res => res && clearInterval(this._))(this._test()), interval)), delay);
   }
 
   _test() {
-    if (this.all) {
+    if (this._all) {
       const res = document.querySelectorAll(this._s);
       if (res.length > 0) {
         return this.resolve([].slice.call(res)), true;
@@ -38,6 +41,10 @@ class Once {
 
 export function onceSome(selector) {
   return new Once(selector, true).promise;
+}
+
+export function onceMore(selector, delay = moreDelay) {
+  return new Once(selector, true, delay).promise;
 }
 
 export function onceAny(selector) {
