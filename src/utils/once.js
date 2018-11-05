c84b0014b229f3375df56abbb9e3b81e0f4fcdee
ccjmne <ccjmne@gmail.com>
2018-11-05 19:31:32 +0100

    Add max retries mechanism to once... utils

--- src/utils/once.js
+++ src/utils/once.js
@@ -1,15 +1,45 @@
 'use strict';
 
-// TODO: Add max retries mechanism
-export function onceAny(selector) {
-  return new Promise(resolve => {
-    const _ = setInterval(() => (e => e && resolve(e) && clearInterval(_))(document.querySelector(selector)), 100);
-  });
+const interval = 50;
+const maxTries = 1000 / interval;
+
+class Once {
+  constructor(selector = '', selectAll = false) {
+    this.all = !!selectAll;
+    this.promise = new Promise((resolve, reject) => {
+      this.resolve = resolve;
+      this.reject = reject;
+    });
+
+    this._s = selector;
+    this._count = 0;
+    return this._test() || (this._ = setInterval(() => (res => res && clearInterval(this._))(this._test()), interval));
+  }
+
+  _test() {
+    if (this.all) {
+      const res = document.querySelectorAll(this._s);
+      if (res.length > 0) {
+        return this.resolve([].slice.call(res)), true;
+      }
+    } else {
+      const res = document.querySelector(this._s);
+      if (res) {
+        return this.resolve(res), true;
+      }
+    }
+
+    if ((++this._count) > maxTries) {
+      clearInterval(this._);
+      this.reject(`Couldn't find ${ this._s } after ${ maxTries } tries.`);
+    }
+  }
 }
 
-// TODO: Add max retries mechanism
 export function onceSome(selector) {
-  return new Promise(resolve => {
-    const _ = setInterval(() => (e => e.length > 0 && resolve([].slice.call(e)) && clearInterval(_))(document.querySelectorAll(selector)), 100);
-  });
+  return new Once(selector, true).promise;
+}
+
+export function onceAny(selector) {
+  return new Once(selector, false).promise;
 }
