3d7a6609d4323eed63bd484a3114cfd30f661586
ccjmne <ccjmne@gmail.com>
2018-11-05 19:34:46 +0100

    Enhance 'create' util to handle attributes

--- src/utils/elements.js
+++ src/utils/elements.js
@@ -6,12 +6,14 @@ export function detach(element) {
   }
 }
 
-export function create({ type = 'div', id = '', classes = [], contents = '' }) {
+export function create({ type = 'div', id = '', classes = [], contents = '', children = [], attributes = {} }) {
   const e = document.createElement(type);
   e.id = id;
   e.classList.add(...classes);
   e.innerHTML = contents;
   e.detach = detach.bind(null, e);
   e.pickStylesFrom = (from, props) => Object.assign(e.style, (s => props.reduce((acc, p) => ({ ...acc, [p]: s[p] }), {}))(window.getComputedStyle(from))); // jshint ignore: line
+  Object.keys(attributes).forEach(k => e.setAttribute(k, attributes[k]));
+  children.forEach(e.appendChild.bind(e));
   return e;
 }
