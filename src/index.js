202de5d202c0fb76aa96376d4f9427da377e782d
ccjmne <ccjmne@gmail.com>
2018-11-06 01:55:51 +0100

    Parse DOM twice for newer results fetched later

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import { create, onceAny, onceSome, getUrlParameter, getOpts } from './utils/module';
+import { create, onceAny, onceSome, onceMore, getUrlParameter, getOpts } from './utils/module';
 import { tooltip, indicator, toggleHelp } from './elements/module';
 import { toggleFilter } from './modules/filter-and-sort';
 import { toggleSwitchTabs } from './modules/switch-tabs';
@@ -55,52 +55,60 @@ getOpts().then(options => {
     (op => typeof op === 'function' && Promise.resolve(e.preventDefault()).then(() => e.stopPropagation()).then(() => op(e)))(opsMap[e.key]);
   }));
 
-  // Once results are listed, additionally handle browsing them
-  onceSome(['#search .r > a:first-of-type', '#search .r g-link:first-of-type > a', '.ads-ad h3 > a:not(:empty)', '.ads-ad a > h3']) // #res h3, .ads-ad h3 maybe?
-    .then(nodes => nodes.filter(e => !e.closest(['g-scrolling-carousel', 'g-accordion-expander']))) // exclude carousel and 'people also ask' results
-    .then(nodes => nodes.filter(e => e.offsetParent !== null)) // exclude invisible elements – e.g.: ads hidden by an ad-blocker (see https://stackoverflow.com/a/21696585)
-    .then((function (nodes) {
-      Object.assign(this, {
+  function init(nodes) {
+    const elements = nodes
+      .filter(e => !e.closest(['g-scrolling-carousel', 'g-accordion-expander'])) // exclude carousel and 'people also ask' results
+      .filter(e => e.offsetParent !== null); // exclude invisible elements – e.g.: ads hidden by an ad-blocker (see https://stackoverflow.com/a/21696585)
+
+    (function (self) {
+      Object.assign(self, {
         prev: document.querySelector('a.pn#pnprev'),
         next: document.querySelector('a.pn#pnnext'),
-        cur: nodes.length > 0 ? 0 : -1,
-        results: nodes.map(x => ({ container: x.closest(['.r', 'li.ads-ad', '.ads-ad li']), palette: x.querySelector('h3') || x.closest('h3'), link: x.closest('a') })),
-        go: e => this.results[this.cur] && this.results[this.cur].link.dispatchEvent(new MouseEvent('click', e)),
+        cur: elements.length > 0 ? 0 : -1,
+        results: elements.map(x => ({ container: x.closest(['.r', 'li.ads-ad', '.ads-ad li']), palette: x.querySelector('h3') || x.closest('h3'), link: x.closest('a') })),
+        go: e => self.results[self.cur] && self.results[self.cur].link.dispatchEvent(new MouseEvent('click', e)),
         focus: idx => (result => {
           result.link.focus();
           result.container.prepend(indicator);
           result.container.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
           indicator.animate([{ transform: 'translateX(-50px)' }, { transform: 'translateX(0)' }], { duration: 100, easing: 'ease-out' });
-        })(this.results[this.cur = idx])
+        })(self.results[self.cur = idx])
       });
 
-      this.results.forEach((result, idx) => {
+      self.results.forEach((result, idx) => {
         const numberIndicator = create({ classes: ['ccjmne-snh-number-indicator'], contents: idx + 1 });
         numberIndicator.pickStylesFrom(result.palette, ['height', 'line-height']);
         numberIndicator.addEventListener('mouseenter', tooltip.reveal);
         result.container.style.position = 'relative';
         result.container.style.overflow = 'visible';
+        result.container.querySelectorAll('.ccjmne-snh-number-indicator').forEach(e => result.container.removeChild(e));
         result.container.prepend(numberIndicator);
       });
 
-      this.focus(this.cur);
-
       // 1 through 9 on the numbers row and/or on the numeric pad -> follow corresponding result
       Object.assign(opsMap, ...[...Array(9).keys()].map(idx => ({
-        [idx + 1]: e => (this.focus(idx), this.go(e))
+        [idx + 1]: e => (self.focus(idx), self.go(e))
       })));
       Object.assign(opsMap, {
-        'restoreFocus': () => this.focus(this.cur > 0 ? this.cur : 0),
-        [open]: /*               -> follow focused  */ e => this.go({ ctrlKey: e.ctrlKey, shiftKey: e.shiftKey }),
-        [open.toUpperCase()]: /* -> follow focused  */ e => this.go({ ctrlKey: e.ctrlKey, shiftKey: e.shiftKey }),
-        'ArrowLeft': /*          -> previous page   */ () => this.prev && this.prev.dispatchEvent(new MouseEvent('click')),
-        'ArrowUp': /*            -> previous result */ () => this.focus(this.cur > 0 ? this.cur - 1 : this.results.length - 1),
-        'ArrowRight': /*         -> next page       */ () => this.next && this.next.dispatchEvent(new MouseEvent('click')),
-        'ArrowDown': /*          -> next result     */ () => this.focus(++this.cur % this.results.length), // jshint -W069
+        'restoreFocus': () => self.focus(self.cur > 0 ? self.cur : 0),
+        [open]: /*               -> follow focused  */ e => self.go({ ctrlKey: e.ctrlKey, shiftKey: e.shiftKey }),
+        [open.toUpperCase()]: /* -> follow focused  */ e => self.go({ ctrlKey: e.ctrlKey, shiftKey: e.shiftKey }),
+        'ArrowLeft': /*          -> previous page   */ () => self.prev && self.prev.dispatchEvent(new MouseEvent('click')),
+        'ArrowUp': /*            -> previous result */ () => self.focus(self.cur > 0 ? self.cur - 1 : self.results.length - 1),
+        'ArrowRight': /*         -> next page       */ () => self.next && self.next.dispatchEvent(new MouseEvent('click')),
+        'ArrowDown': /*          -> next result     */ () => self.focus(++self.cur % self.results.length), // jshint -W069
         [udlr.charAt(0)]: /*     -> previous result */ () => opsMap['ArrowUp'](),
         [udlr.charAt(1)]: /*     -> next result     */ () => opsMap['ArrowDown'](),
         [udlr.charAt(2)]: /*     -> previous page   */ () => opsMap['ArrowLeft'](),
         [udlr.charAt(3)]: /*     -> next page       */ () => opsMap['ArrowRight']() // jshint +W069
       });
-    }).bind({}));
+    })({});
+  }
+
+  // Once results are listed, additionally handle browsing them
+  onceSome(['#search .r > a:first-of-type', '#search .r g-link:first-of-type > a', '.ads-ad h3 > a:not(:empty)', '.ads-ad a > h3']) // #res h3, .ads-ad h3 maybe?
+    .then(init) // ASAP, init w/ first results available
+    .then(() => opsMap.restoreFocus())
+    .then(() => onceMore(['#search .r > a:first-of-type', '#search .r g-link:first-of-type > a', '.ads-ad h3 > a:not(:empty)', '.ads-ad a > h3']))
+    .then(init); // 2nd pass, w/ potentially more results loaded shortly after the page was ready
 });
